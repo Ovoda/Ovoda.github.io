d6a8277a5ee0bbfa937540497a1bcd94031dbc4b
Ovoda <cdetourrispro@gmail.com>
2022-04-05 13:08:21 +0200

    WIP

--- index.js
+++ index.js
@@ -10,24 +10,37 @@ function updateValue(e) {
         document.getElementById("2").style.opacity = "30%";
         document.getElementById("3").style.opacity = "30%";
         document.getElementById("4").style.opacity = "30%";
-        document.getElementById("rarity_description").innerHTML = "Common NFT<br /> this is a description of what you will get. Enjoy !";
+        document.getElementById("rarity_description").innerHTML = "Common NFT<br /> The first step to donation, good job you !";
     } else if (e.target.value <= 100 && e.target.value > 50) {
         document.getElementById("1").style.opacity = "30%";
         document.getElementById("2").style.opacity = "100%";
         document.getElementById("3").style.opacity = "30%";
         document.getElementById("4").style.opacity = "30%";
-        document.getElementById("rarity_description").innerHTML = "Rare NFT<br /> this is a description of what you will get. Enjoy !";
+        document.getElementById("rarity_description").innerHTML = "Rare NFT<br /> A nice NFT for a nice donation, thank you !";
     } else if (e.target.value <= 200 && e.target.value > 100) {
         document.getElementById("1").style.opacity = "30%";
         document.getElementById("2").style.opacity = "30%";
         document.getElementById("3").style.opacity = "100%";
         document.getElementById("4").style.opacity = "30%";
-        document.getElementById("rarity_description").innerHTML = "Epic NFT<br /> this is a description of what you will get. Enjoy !";
+        document.getElementById("rarity_description").innerHTML = "Epic NFT<br /> Well, that's an epic move right there. Thanks champ !";
     } else if (e.target.value > 200) {
         document.getElementById("1").style.opacity = "30%";
         document.getElementById("2").style.opacity = "30%";
         document.getElementById("3").style.opacity = "30%";
         document.getElementById("4").style.opacity = "100%";
-        document.getElementById("rarity_description").innerHTML = "Metafunds Master NFT<br /> this is a description of what you will get. Enjoy !";
+        document.getElementById("rarity_description").innerHTML = "Metafunds King NFT<br /> King of Metfunds, we salute you.";
     }
 }
+
+checkboxes = document.getElementsByClassName("checkbox_container");
+help_box = document.getElementById("help_box");
+
+checkboxes[0].addEventListener("mouseenter", function (event) {
+    help_box.innerText = "You want to donate the NFT to the NGO.\n This is very genereous of you.";
+}, false);
+checkboxes[1].addEventListener("mouseenter", function (event) {
+    help_box.innerText = "You want to mint the NFT on your own wallet";
+}, false);
+checkboxes[2].addEventListener("mouseenter", function (event) {
+    help_box.innerText = "We store the NFT for you, you get it back whenever you want";
+}, false);
