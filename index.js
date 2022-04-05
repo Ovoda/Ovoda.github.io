69e1819dc20fabc3edcbc62f223617f9ab846465
Ovoda <cdetourrispro@gmail.com>
2022-04-05 16:24:49 +0200

    wip

--- index.js
+++ index.js
@@ -34,13 +34,70 @@ function updateValue(e) {
 
 checkboxes = document.getElementsByClassName("checkbox_container");
 help_box = document.getElementById("help_box");
+info_needed_box = document.getElementById("info_needed_box");
 
-checkboxes[0].addEventListener("mouseenter", function (event) {
+function reset_info_needed_box() {
+    var child = document.getElementById("input_wallet");
+    if (child)
+        info_needed_box.removeChild(child);
+}
+
+
+checkboxes[0].addEventListener("click", function (event) {
     help_box.innerText = "You want to donate the NFT to the NGO.";
+    reset_info_needed_box();
+    checkboxes[0].classList.add("checkbox_container_selected");
+    checkboxes[1].classList.remove("checkbox_container_selected");
+    checkboxes[2].classList.remove("checkbox_container_selected");
 }, false);
-checkboxes[1].addEventListener("mouseenter", function (event) {
+
+checkboxes[1].addEventListener("click", function (event) {
     help_box.innerText = "You want to mint the NFT on your own wallet";
+
+    reset_info_needed_box();
+    const input_wallet = document.createElement('input');
+    input_wallet.placeholder = "Metamask address";
+    input_wallet.id = "input_wallet";
+    input_wallet.className = "text_input";
+    document.getElementById("info_needed_box").appendChild(input_wallet);
+
+    checkboxes[0].classList.remove("checkbox_container_selected");
+    checkboxes[1].classList.add("checkbox_container_selected");
+    checkboxes[2].classList.remove("checkbox_container_selected");
 }, false);
-checkboxes[2].addEventListener("mouseenter", function (event) {
+
+checkboxes[2].addEventListener("click", function (event) {
     help_box.innerText = "We store the NFT for you, you get it back whenever you want";
+
+    reset_info_needed_box();
+
+    checkboxes[0].classList.remove("checkbox_container_selected");
+    checkboxes[1].classList.remove("checkbox_container_selected");
+    checkboxes[2].classList.add("checkbox_container_selected");
 }, false);
+
+nexts = document.getElementsByClassName("next");
+
+nexts[1].addEventListener("click", function (event) {
+    step = document.getElementById("step_1");
+    step.style.background = "linear-gradient(0.15turn, #06DBAC, #BD00FF)";
+
+});
+
+nexts[2].addEventListener("click", function (event) {
+    step = document.getElementById("step_2");
+    step.style.background = "linear-gradient(0.15turn, #06DBAC, #BD00FF)";
+
+});
+
+nexts[3].addEventListener("click", function (event) {
+    step = document.getElementById("step_3");
+    step.style.background = "linear-gradient(0.15turn, #06DBAC, #BD00FF)";
+
+});
+
+nexts[4].addEventListener("click", function (event) {
+    step = document.getElementById("step_4");
+    step.style.background = "linear-gradient(0.15turn, #06DBAC, #BD00FF)";
+
+});
