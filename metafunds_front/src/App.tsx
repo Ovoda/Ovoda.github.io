30f2bb458d8fa6c9ae35cdd8084701fa3239dc54
Ovoda <cdetourrispro@gmail.com>
2022-04-10 00:26:57 +0200

    front: responsive navbar WIP

--- metafunds_front/src/App.tsx
+++ metafunds_front/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Background />
       <Navbar />
-      {/* <a href="#" className="button">Metadonate</a>
+      <a href="#" className="button">Metadonate</a>
       <a href="#" className="button">Contact US</a>
       <input type="text" className="text_input" placeholder='email' />
       <h1>Title H1</h1>
@@ -21,7 +21,7 @@ function App() {
       <h5>Title H5</h5>
       <h6>Title H6</h6>
       <p>This is some p tag text</p>
-      <strong>This is some p tag strong text</strong> */}
+      <strong>This is some p tag strong text</strong>
     </div >
   );
 }
